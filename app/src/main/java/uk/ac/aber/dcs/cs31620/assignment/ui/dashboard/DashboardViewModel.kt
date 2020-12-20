c63ad6ae6d77f3227c173f39a41c5ab2a65387d9
Dawid Wozniak <daw79@aber.ac.uk>
2020-12-20 15:24:20 +0000

    build: Add first version

---
+++ app/src/main/java/uk/ac/aber/dcs/cs31620/assignment/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package uk.ac.aber.dcs.cs31620.assignment.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
