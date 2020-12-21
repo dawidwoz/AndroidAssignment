93a68d5653882a2ffd790ec539161c6d2fb975e0
Dawid Wozniak <daw79@aber.ac.uk>
2020-12-21 00:15:06 +0000

    feat(ui): Add basic UI and color schema

--- app/src/main/java/uk/ac/aber/dcs/cs31620/assignment/MainActivity.kt
+++ app/src/main/java/uk/ac/aber/dcs/cs31620/assignment/MainActivity.kt
@@ -4,20 +4,28 @@ import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import uk.ac.aber.dcs.cs31620.assignment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+        val toolbar = binding.toolbar
+        setSupportActionBar(toolbar)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val navView: BottomNavigationView = binding.navView
         val appBarConfiguration = AppBarConfiguration(setOf(
                 R.id.navigation_home, R.id.navigation_list, R.id.navigation_learn, R.id.navigation_test, R.id.navigation_settings))
         setupActionBarWithNavController(navController, appBarConfiguration)
