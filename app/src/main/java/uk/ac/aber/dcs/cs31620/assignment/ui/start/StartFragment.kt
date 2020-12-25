ca2093adcb3e53d93e308fe3e60503a2cf607baa
Dawid Wozniak <daw79@aber.ac.uk>
2020-12-25 14:51:36 +0000

    feat(start_screen): Add basic form validation

--- app/src/main/java/uk/ac/aber/dcs/cs31620/assignment/ui/start/StartFragment.kt
+++ app/src/main/java/uk/ac/aber/dcs/cs31620/assignment/ui/start/StartFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -11,7 +13,6 @@ import androidx.navigation.NavOptions
 import androidx.navigation.fragment.findNavController
 import uk.ac.aber.dcs.cs31620.assignment.MainActivity
 import uk.ac.aber.dcs.cs31620.assignment.R
-import uk.ac.aber.dcs.cs31620.assignment.databinding.ActivityMainBinding
 import uk.ac.aber.dcs.cs31620.assignment.databinding.FragmentStartBinding
 
 
@@ -19,27 +20,48 @@ class StartFragment : Fragment() {
 
     private lateinit var startViewModel: StartViewModel
     private lateinit var binding: FragmentStartBinding
-    private lateinit var bindingGlobal: ActivityMainBinding
+    private lateinit var yourLanguage: EditText
+    private lateinit var desiredLanguage: EditText
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): ConstraintLayout {
         binding = FragmentStartBinding.inflate(layoutInflater)
         startViewModel = ViewModelProvider(this).get(StartViewModel::class.java)
+        yourLanguage = binding.editOwnLanguage
+        desiredLanguage = binding.editForeignLanguage
 
         MainActivity.UiController.hideBottomNav()
         MainActivity.UiController.hideToolbar()
 
         var buttonNext = binding.button;
         buttonNext.setOnClickListener(View.OnClickListener {
-            var navController =  findNavController()
-            val navOption = NavOptions.Builder().setPopUpTo(R.id.navigation_start, true).build()
-            navController.navigate(R.id.action_start_to_home, null, navOption)
-            MainActivity.UiController.showBottomNav()
-            MainActivity.UiController.showToolbar()
+            if (validateForm()) {
+                saveAndGoHome()
+            } else {
+                displayToastError()
+            }
         })
         return binding.root
+    }
+
+    private fun validateForm(): Boolean {
+        return yourLanguage.text.isNotEmpty() && desiredLanguage.text.isNotEmpty()
+    }
+
+    private fun displayToastError() {
+        val text = R.string.validation_error;
+        val toast = Toast.makeText(requireContext(), text, Toast.LENGTH_LONG)
+        toast.show()
+    }
+
+    private fun saveAndGoHome() {
+        val navController = findNavController()
+        val navOption = NavOptions.Builder().setPopUpTo(R.id.navigation_start, true).build()
+        navController.navigate(R.id.action_start_to_home, null, navOption)
+        MainActivity.UiController.showBottomNav()
+        MainActivity.UiController.showToolbar()
     }
 }
