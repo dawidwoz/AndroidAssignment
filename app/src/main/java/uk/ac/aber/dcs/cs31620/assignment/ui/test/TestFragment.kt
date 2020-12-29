03fe667418e0bef92ae7de1438c7a07530227d94
Dawid Wozniak <daw79@aber.ac.uk>
2020-12-29 01:53:19 +0000

    feat(fragment_test): Add very first UI

--- app/src/main/java/uk/ac/aber/dcs/cs31620/assignment/ui/test/TestFragment.kt
+++ app/src/main/java/uk/ac/aber/dcs/cs31620/assignment/ui/test/TestFragment.kt
@@ -4,24 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import uk.ac.aber.dcs.cs31620.assignment.R
+import uk.ac.aber.dcs.cs31620.assignment.databinding.FragmentTestBinding
 
 class TestFragment : Fragment() {
 
-    private lateinit var testViewModel: TestViewModel
+    private lateinit var binding : FragmentTestBinding
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        testViewModel =
-                ViewModelProvider(this).get(TestViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_test, container, false)
-        return root
+        binding = FragmentTestBinding.inflate(layoutInflater)
+        return binding.root
     }
 }
