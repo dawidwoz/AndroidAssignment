ffa1122dd0527664cffdc3ce1fb4943a1aa0ab4b
Dawid Wozniak <daw79@aber.ac.uk>
2020-12-31 22:38:26 +0000

    feat(fragment_learn): Add fragment_learn

--- app/src/main/java/uk/ac/aber/dcs/cs31620/assignment/ui/learn/LearnFragment.kt
+++ app/src/main/java/uk/ac/aber/dcs/cs31620/assignment/ui/learn/LearnFragment.kt
@@ -4,28 +4,57 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.observe
 import uk.ac.aber.dcs.cs31620.assignment.R
+import uk.ac.aber.dcs.cs31620.assignment.databinding.FragmentLearnBinding
+import uk.ac.aber.dcs.cs31620.assignment.model.Word
+import uk.ac.aber.dcs.cs31620.assignment.model.WordViewModel
 
 class LearnFragment : Fragment() {
 
-    private lateinit var learnViewModel: LearnViewModel
+    private lateinit var binding: FragmentLearnBinding
+    private lateinit var wordsViewModel: WordViewModel
+    private lateinit var buttonLearn: Button
+    private lateinit var textLearn: TextView
+    private var shouldGetNewWord: Boolean = false
+    private var currentWord : Word? = null
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        learnViewModel =
-                ViewModelProvider(this).get(LearnViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_learn, container, false)
-        val textView: TextView = root.findViewById(R.id.text_notifications)
-        learnViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        binding = FragmentLearnBinding.inflate(layoutInflater)
+        wordsViewModel = ViewModelProvider(this).get(WordViewModel::class.java)
+
+        buttonLearn = binding.learnButtonWord
+        textLearn = binding.learnQuestion
+
+        getNewWord()
+
+        buttonLearn.setOnClickListener {
+            if (!shouldGetNewWord) {
+                getNewWord()
+            } else {
+                textLearn.text = requireContext().getString(R.string.learn_question_new_word)
+                buttonLearn.text = currentWord?.translation
+                shouldGetNewWord = false
+            }
+        }
+
+        return binding.root
+    }
+
+    private fun getNewWord() {
+        wordsViewModel.getRandomWords(1).observe(viewLifecycleOwner) {
+            currentWord = it.getOrNull(0)
+            textLearn.text = requireContext().getString(R.string.learn_question_translation)
+            buttonLearn.text = currentWord?.original
+            shouldGetNewWord = true
+        }
     }
 }
