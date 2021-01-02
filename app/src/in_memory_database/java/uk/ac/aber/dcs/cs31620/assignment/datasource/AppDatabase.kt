b4c8758fc2565358150a409cd9605dbf13ea4abb
Dawid Wozniak <daw79@aber.ac.uk>
2021-01-02 11:47:58 +0000

    test: Add grade build variants

---
+++ app/src/in_memory_database/java/uk/ac/aber/dcs/cs31620/assignment/datasource/AppDatabase.kt
@@ -0,0 +1,48 @@
+package uk.ac.aber.dcs.cs31620.assignment.datasource
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import uk.ac.aber.dcs.cs31620.assignment.model.*
+
+@Database(entities = [Language::class, Word::class], version = 1)
+abstract class AppDatabase : RoomDatabase(), RoomDatabaseInterface {
+
+    abstract override fun languagesDao(): LanguageDao
+
+    abstract override fun wordDao(): WordDao
+
+    override fun closeDb() {
+        instance?.close()
+        instance = null
+    }
+
+    companion object {
+        private var instance: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase? {
+            synchronized(this) {
+                if (instance == null) {
+                    instance =
+                        Room.inMemoryDatabaseBuilder<AppDatabase>(
+                            context.applicationContext,
+                            AppDatabase::class.java
+                        )
+                            .addCallback(roomDatabaseCallback(context))
+                            .build()
+                }
+                return instance!!
+            }
+        }
+
+        private fun roomDatabaseCallback(context: Context): Callback {
+            return object : Callback() {
+                override fun onCreate(db: SupportSQLiteDatabase) {
+                    super.onCreate(db)
+                }
+            }
+        }
+    }
+}
